66a18323945f78aa50d6fc52494aef044b55a2f7
Szymon Gałecki <szymongalecki@gmail.com>
2023-04-01 20:13:33 +0200

    scrapes all apartment data for given boligzonen.dk search url

--- scrape.py
+++ scrape.py
@@ -1,31 +1,33 @@
+import re
 import requests
 from bs4 import BeautifulSoup
 
 # test apartments
-# url = "https://boligzonen.dk/lejeboliger/dejlig-lejlighed-pa-torvegade"
-# url = "https://boligzonen.dk/lejeboliger/1-vaerelses-lejlighed-i-frederiksberg-0643ce75-2f8e-4311-9e2a-46ad9bfb3ad8"
-url = "https://boligzonen.dk/lejeboliger/villalejlighed-frederiksberg-gratis-parkering-naer-metro"
+# url = "https://boligzonen.dk/lejeboliger/villalejlighed-frederiksberg-gratis-parkering-naer-metro"
+url = "https://boligzonen.dk/lejebolig/kobenhavn-kommune"
 
 
 def to_int(number: str) -> int:
+    """custom int parser"""
     return int("".join([_ for _ in number if _.isnumeric()]))
 
 
 def soup(url: str) -> BeautifulSoup:
+    """creates soup for given url"""
     response = requests.get(url)
     content = response.content
     return BeautifulSoup(content, "html.parser")
 
 
-def page_of_apartments():
-    links = links_to_apartments()
+def page_of_apartments(links: list) -> None:
+    """prints out data for every apartment"""
     for l in links:
         print(apartment_data(soup(l)))
 
 
-def links_to_apartments() -> list:
+def apartments_on_page(s: BeautifulSoup) -> list:
+    """returns links for all the apartments on page"""
     root = "https://boligzonen.dk"
-    s = soup("https://boligzonen.dk/lejebolig/kobenhavn-kommune")
     apartments = s.find("div", class_="row small-gutters properties").find_all(
         "a", class_="property-partial"
     )
@@ -33,8 +35,21 @@ def links_to_apartments() -> list:
     return links
 
 
+def last_page(s: BeautifulSoup) -> int:
+    """returns the number of the last page for given search soup"""
+    rel = s.find("span", class_="last").find("a", href=True)["href"]
+    last = int(re.search("page=[0-9]+", rel).group().strip("page="))
+    return last
+
+
+def next_page(url: str, page: int) -> str:
+    """constructs url for the next page of a given search"""
+    link = f"{url}?page={page}"
+    return link
+
+
 def apartment_data(s: BeautifulSoup) -> dict:
-    # find and parse
+    """finds, parses and returns desired data of a single apartment"""
     address = s.find(name="p", class_="address-line").contents
     street = address[0].strip(" ,")
     zip_code = int(address[-1].split()[0])
@@ -45,7 +60,6 @@ def apartment_data(s: BeautifulSoup) -> dict:
     latitude = float(location["data-lat"])
     longitude = float(location["data-lng"])
 
-    # return apartment data
     return {
         "rooms": rooms,
         "area": area,
@@ -57,4 +71,13 @@ def apartment_data(s: BeautifulSoup) -> dict:
     }
 
 
-page_of_apartments()
+def scrape_all(url: str) -> None:
+    """scrapes all pages of boligzonen.dk for given search"""
+    s = soup(url)
+    last = last_page(s)
+    for i in range(1, last + 1):
+        page_of_apartments(apartments_on_page(soup(next_page(url, i))))
+
+
+if __name__ == "__main__":
+    scrape_all(url)
